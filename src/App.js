631481a3d274f0c661e2d93e7e9e1d89c149261b
KhiaJEO <khiajeo@gmail.com>
2022-04-09 12:17:43 +0800

    Finished Todo example
    Add a complete todo and remove item action

--- src/App.js
+++ src/App.js
@@ -1,37 +1,62 @@
 import {useState} from "react";
 
 // Components
-import TodoItem from "./components/TodoItem";
-import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoForm from "./components/TodoForm/TodoForm";
+
+// CSS
+import "./App.css"
 
 function App() {
   // State
   const [todos, setTodos] = useState([
-    "Learn React",
-    "Tell all my friends to learn React",
-    "Get them to tell five of their friends to learn React",
-    "Start a successful mlm or pyramid scheme around React",
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet with Daisy for workshop", isCompleted: false },
+    { text: "Meet with Jordi", isCompleted: false },
   ]);
 
-  // Actions ("..." is the spread operator - looping through old array copying each element and adding it to the new array)
-  const addTodo = (text) => {
-    const newTodos = [...todos, text];
+  // Actions 
+    // ("..." is the spread operator - looping through old array copying each element and adding it to the new array)
     //  What it would look like if you did not add the three dots before todos
     // [["something", "something else"], "some todo"]
     // Using the spread operator it looks like this: 
     // ["something", "something else", "some todo"]
+  
+    const addTodo = (text) => {
+    const newTodos = [...todos, { text:text, isComplete: false }];
     setTodos(newTodos);
   };
 
+  // fin dthe todo in this array and take its isComplete value and set it to true (whether its false or true)
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  };
+
   return (
-    <div>
+    <div className="app">
+      <div className="todo-list">
       <h1>My todo list</h1>
-      <TodoItem/>
       {todos.map((todo, index) => (
-        <TodoItem todo={todo} key={index} />
+        <TodoItem 
+          todo={todo} 
+          key={index} 
+          index={index} 
+          completeTodo={completeTodo} 
+          removeTodo={removeTodo}
+        />
       ))}
-      <TodoForm addTodo={addTodo} />
+    <TodoForm addTodo={addTodo} />
     </div>
+    </div>
+  
   );
 }
 
---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,25 @@
+
+import React from "react";
+
+// CSS
+import "./TodoItem.css";
+
+function TodoItem(props) {
+    const { todo, index, completeTodo, removeTodo } = props;
+
+    return (
+    <div className={`todo ${todo.isCompleted ? "complete" : ""}`}> 
+    {todo.text} 
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>X</button>
+        </div>
+    </div>
+);
+}
+
+export default TodoItem;
+
+
+// ? is a turnery statement (a short hand for an if else statement)
+// : is an else statememt
